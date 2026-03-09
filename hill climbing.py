2fbdd8e96c1c7ee88bafc8eed8bba24c2390b8c1
Lakshmidhar <lakshmidhar2007@gmail.com>
2026-03-09 10:36:41 +0530

    All the changes are over

--- hill climbing.py
+++ hill climbing.py
@@ -1,47 +1,43 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import random
 import string
-def generate_random_solution(answer):
-    l=TYPE THE CODE HERE ,FIND THE LEN OF answer and store in l
-    return [random.choice(string.printable) for _ in range(l)]
-def evaluate(solution,answer):
-    print(solution)
-    target=list(answer)
-    diff=0
-    for i in range(len(target)):
-        s=solution[i]
-        t=target[i]
-        #to calculate the "difference" between two strings, character by character.
-        #ord(s) - ord(t) calculates the difference between the ASCII values of the characters s and t.
-         #abs() takes the absolute value of this difference to ensure that it is non-negative. This is important because the difference could be negative if s is less than t in terms of ASCII value.
-         #The absolute value ensures that the difference is always positive or zero.
-        diff +=Type the code here by referring to the above description 
-    return diff
-def mutate_solution(solution):
-    ind=random.randint(0,len(solution)-1)
-    solution[ind]=random.choice(string.printable)
-    return solution
-def SimpleHillClimbing():
-    answer="Artificial Intelligence"
-    best=generate_random_solution(answer)
-    best_score=evaluate(best,answer)
-    while True:
-        print("Score:",best_score," Solution : ","".join(best))  
-        if best_score==0:
-            break
-        new_solution=mutate_solution(list(best))
-        score=evaluate(new_solution,answer)   
-        if score<best_score:
-            best=new_solution
-            best_score=score
-#answer="Artificial Intelligence"
-#print(generate_random_solution(answer))
-#solution=generate_random_solution(answer)
-#print(evaluate(solution,answer))
-SimpleHillClimbing()
 
+def generate_random_solution(ans):
+    l = len(ans)
+    return [random.choice(string.printable) for _ in range(l)]
+
+def mutate(soln):
+    ind = random.randint(0, len(soln) - 1)
+    soln[ind] = random.choice(string.printable)
+    return soln
+
+def evaluate(solution, answer):
+    diff = 0
+    for i in range(len(solution)):
+        diff += abs(ord(answer[i]) - ord(solution[i]))
+    return diff
+
+def simplehillclimbing():
+    answer = input("Enter target string: ")
+    best = generate_random_solution(answer)
+    best_score = evaluate(best, answer)
+    iterations = 0
+    max_iterations = 100000
+
+    while best_score != 0 and iterations < max_iterations:
+        print("Score:", best_score, "String:", ''.join(best))
+        new_soln = mutate(best.copy())
+        score = evaluate(new_soln, answer)
+
+        if score < best_score:
+            best = new_soln
+            best_score = score
+
+        iterations += 1
+
+    if best_score == 0:
+        print("\nTarget Found:", ''.join(best))
+    else:
+        print("\nStopped after max iterations.")
+        print("Best found:", ''.join(best))
+
+simplehillclimbing()
